fd79ed584a20ff30b3f6ce815bc613853aca293b
pasindualawattegama000 <pasindu.20200732@iit.ac.lk>
2025-05-24 19:17:18 +0530

    test host

--- config.py
+++ config.py
@@ -7,7 +7,12 @@ class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'your-secret-key-here')
     
     # MySQL configuration
-    DB_HOST = os.getenv('DB_HOST', 'localhost')
-    DB_DATABASE = os.getenv('DB_DATABASE', 'greenhouse')
-    DB_USER = os.getenv('DB_USER', 'root')
-    DB_PASSWORD = os.getenv('DB_PASSWORD', 'password')
+    # DB_HOST = os.getenv('DB_HOST', 'localhost')
+    # DB_DATABASE = os.getenv('DB_DATABASE', 'greenhouse')
+    # DB_USER = os.getenv('DB_USER', 'root')
+    # DB_PASSWORD = os.getenv('DB_PASSWORD', 'password')
+
+    DB_HOST = 'procodex.mysql.pythonanywhere-services.com'
+    DB_DATABASE = 'procodex$default'
+    DB_USER = 'procodex'
+    DB_PASSWORD = 'QQ99XBCDL'
